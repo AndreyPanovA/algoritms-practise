7af437a6e54ce2f5089f2d1474d7e0c8e60d1c19
AndreyPanovA <a.andrey.panov@gmail.com>
2021-10-22 22:09:53 +0300

    icon-standalone

--- PWA/panov/pages/_app.tsx
+++ PWA/panov/pages/_app.tsx
@@ -1,6 +1,17 @@
+
+import Layout from "../components/Layout";
+import Link from "next/link";
+
 const App = ()=> {
     return (
-        <div>cool</div>
+        <Layout title="Home | Next.js + TypeScript Example">
+            <h1>Hello Next.js 👋</h1>
+            <p>
+                <Link href="/about">
+                    <a>Abouqweqwe12123tw</a>
+                </Link>
+            </p>
+        </Layout>
     )
 }
 export default App
